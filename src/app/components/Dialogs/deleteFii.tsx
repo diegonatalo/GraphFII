0492bba14d82ab5e51bc35d5cf694935404c7ec9
Diego Natalo <diegonatalo@gmail.com>
2024-10-03 14:16:15 -0300

    update files

--- src/app/components/Dialogs/deleteFii.tsx
+++ src/app/components/Dialogs/deleteFii.tsx
@@ -17,8 +17,8 @@ export const DeleteDialog = ({ ticker }: DeleteDialogProps) => {
         <Trash size={24} />
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className="data-[state=open]:animate-overlayShow fixed inset-0 bg-black/70" /> 
-        <Dialog.Content className="data-[state=open]:animate-contentShow fixed left-[50%] top-[50%] flex max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] flex-col gap-16 rounded-lg bg-zinc-900 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+        <Dialog.Overlay className="fixed inset-0 bg-black/70 data-[state=open]:animate-overlayShow" />
+        <Dialog.Content className="fixed left-[50%] top-[50%] flex max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] flex-col gap-16 rounded-lg bg-zinc-900 p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none data-[state=open]:animate-contentShow">
           <div className="flex flex-col gap-1">
             <Dialog.Title className="text-xl font-bold text-zinc-300">
               Deletar ativo
